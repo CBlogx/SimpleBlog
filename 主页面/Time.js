a80658b52c998b1deb7b49f734019321fadb933c
CBlogx <CBlogx@github.com>
2020-11-08 12:21:10 +0800

    update

--- 主页面/Time.js
+++ 主页面/Time.js
@@ -13,44 +13,65 @@ function Time(){
     else{
         datetime +=d.getDate();
     }
-    if(d.getMinutes()<10){
-        var time=d.getHours()+":0"+d.getMinutes()+":"+d.getSeconds();
+    if(d.getHours()<10){
+        var time = "0"+d.getHours();
     }
     else{
-        var time=d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
+        var time = d.getHours();
+    }
+    if(d.getMinutes()<10){
+         time+=":0"+d.getMinutes();
+    }
+    else{
+         time+=":"+d.getMinutes();
     }
     if(d.getSeconds()<10){
-        time=d.getHours()+":"+d.getMinutes()+":0"+d.getSeconds();
+        time+=":0"+d.getSeconds();
     }
     else{
-        time=d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
+        time+=":"+d.getSeconds();
     }
-    document.getElementById("Time").innerHTML=datetime+" "+time; 
+    document.getElementById("Time").innerText=datetime+" "+time; 
 }
 
-var Index = 0;
-var Sindex= 1;
-
-function Currentslide(n) {showSlide(Sindex = n);}
+var Index = 0,Sindex= 1,Indextime;
+function Currentslide(n) {
+  showSlide(Sindex = n);
+  clearTimeout(Indextime);
+  if(!showSlide(Sindex=n)){
+    Index--;
+    setTimeout(showSlides,10000);
+  }
+}
 function showSlides(){
-    var i;
+    var i,j;
     var slides = document.getElementsByClassName("Slidepic");
     var dots=document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"; 
     }
+    for(j=0;j<dots.length;j++){
+        dots[j].className="dot";
+    }
     Index++;
     if(Index>slides.length){Index=1;}
     slides[Index-1].style.display = "block"; 
-    setTimeout(showSlides,3000);
+    dots[Index-1].className = "dot active";
+    Indextime=setTimeout(showSlides,3000);
 }
 function showSlide(n){
-  var i;
+  var i,j;
   var slides = document.getElementsByClassName("Slidepic");
+  var dots=document.getElementsByClassName("dot");
   if (n > slides.length) {Sindex = 1} 
   if (n < 1) {Sindex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
   }
+  for(j=0;j<dots.length;j++){
+    dots[j].className="dot";
+  }
+  dots[Sindex-1].className="dot active";
   slides[Sindex-1].style.display = "block"; 
 }
+
